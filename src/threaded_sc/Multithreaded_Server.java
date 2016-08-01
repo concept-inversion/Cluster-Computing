81742b88871f9d00aa5a19a8f6f58837e2d6f3a4
Santosh <Santosh@inversion>
2016-08-02 01:14:56 +0545

    Small change: User input value and argument passing to threads

--- src/threaded_sc/Multithreaded_Server.java
+++ src/threaded_sc/Multithreaded_Server.java
@@ -1,66 +1,42 @@
 package threaded_sc;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
+import javax.swing.*;
 
 public class Multithreaded_Server {
 
-    private static int port = 11111, maxConnections = 5;
-    // Listen for incoming connections and handle them
+    private static int port = 11111, maxConnections = 2;
+     private static Date date = new Date();
+    List<String> syncal = 
+         Collections.synchronizedList(new ArrayList<String>());
+// Listen for incoming connections and handle them
 
     public static void main(String[] args) {
-        int i = 0;
-
-        try {
-            ServerSocket listener = new ServerSocket(port);
-            Socket server;
-
-            while ((i++ < maxConnections) || (maxConnections == 0)) {
-                // doComms connection;
-
-                server = listener.accept();
-                doComms conn_c = new doComms(server);
-                Thread t = new Thread(conn_c);
-                t.start();
-            }
-        } catch (IOException ioe) {
-            System.out.println("IOException on socket listen: " + ioe);
-            ioe.printStackTrace();
-        }
-    }
-
-}
-
-class doComms implements Runnable {
-
-    private Socket server;
-    private String line, input;
-
-    doComms(Socket server) {
-        this.server = server;
-    }
-
-    public void run() {
-
-        input = "";
-
-        try {
-            // Define stream for read and write
-            BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            PrintStream out = new PrintStream(server.getOutputStream());
-
-            //get thread id
-            float threadid = Thread.currentThread().getId();
-            float id = threadid - 9; //starting threadid for java application in 9
-            //send connection msg to client '
-            out.println("You are connected.Start communication  thread_id in server: \t" + threadid + " (" + id + ")");
-            /*
+        int id = 0;
+        System.out.println("Notice: [" + date.toString() + "] Server started!");
+        JFrame frame = new JFrame("Notice: [" + date.toString() + "] Server started");
+         frame.setSize(400,400);
+        //JPanel panel = new JPanel();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame.setDefaultLookAndFeelDecorated(true);
+         JLabel label=new JLabel("HUnter", JLabel.CENTER); 
+        JTextField v0TextField = new JTextField ("initial velocity");
+        frame.add(label);
+        frame.setVisible(true);
+        
+                    
         System.out.print("Enter the Starting  no.          :");  
          Scanner in1 = new Scanner(System.in); 
          float start = in1.nextFloat();
@@ -70,9 +46,65 @@ class doComms implements Runnable {
         
           System.out.print("Enter the no of iterations.: ");
           float step =in1.nextFloat();       
-             */
-            float thread = 5;
-            float start = 0, end = 1, step = 1000;
+             
+        
+ 
+        
+        
+        try {
+            ServerSocket listener = new ServerSocket(port);
+            Socket server;
+
+            while ((id < maxConnections) || (maxConnections == 0)) {
+                // client listner and thread connection;
+
+                server = listener.accept(); 
+                work connector = new work(server,id,maxConnections,start,end,step); // new object of class work , put arguments here
+                Thread t = new Thread(connector);  // create new thread using that object
+                t.start();                      //start the thread 
+                System.out.println("Notice: [" + date.toString() + "] - Client connected! ID: " + id);
+                id++;
+            }
+        } catch (IOException ioe) {
+            System.out.println("IOException on socket listen: " + ioe);
+            ioe.printStackTrace();
+        }
+    }
+
+}
+
+class work implements Runnable {
+
+    private Socket server;
+    private int id,thread;
+    //private String line, input;
+    private float start,end,step;
+    
+    work(Socket server,int id,int thread,float start,float end,float step) {                      //constructor
+        this.server = server;
+        this.id=id;
+        this.end=end;
+        this.start=start;
+        this.step=step;
+        this.thread=thread;
+    }
+
+    public void run() {
+
+      
+        try {
+            // Define stream for read and write
+            BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            PrintStream out = new PrintStream(server.getOutputStream());
+
+            //get thread id
+            float threadid = Thread.currentThread().getId();
+            
+            //send connection msg to client '
+            out.println("You are connected.Start communication  thread_id in server: \t" + threadid + " (" + id + ")");
+
+      //      float thread = 5;
+        //    float start = 0, end = 1, step = 1000;
             float h = (end - start) / step;
             float c_in = step / thread;    // 2 is max thread , c_in is client size
 
